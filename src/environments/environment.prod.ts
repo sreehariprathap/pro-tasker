26eaf65f8203f717e53ed7f374d26d0a6f81bc6f
Sreehari Prathap <sreehariprathap1996@gmail.com>
2023-04-10 23:25:21 +0530

    env updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'no-need',
+  firebaseConfig: {
+    apiKey: 'AIzaSyB7v9GX-F2WiOuTa97-Tdx8LIYscy7piws',
+    authDomain: 'pro-tasker-4e990.firebaseapp.com',
+    projectId: 'pro-tasker-4e990',
+    storageBucket: 'pro-tasker-4e990.appspot.com',
+    messagingSenderId: '635975746967',
+    appId: '1:635975746967:web:212a8375ff2a12f1bab0d2',
+    measurementId: 'G-L5HXGG21JD',
+  },
 };
